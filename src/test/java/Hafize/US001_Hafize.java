036fa71222296622cd3fab1110d9f5a283fb4b15
Hafize Aksoy <hafize.teacher@gmail.com>
2021-07-07 16:41:32 -0700

    hafize

---
+++ src/test/java/Hafize/US001_Hafize.java
@@ -0,0 +1,10 @@
+package Hafize;
+
+public class US001_Hafize {
+
+    public static void main(String[] args) {
+
+        System.out.println("work 1");
+        System.out.println("work 2");
+    }
+}
